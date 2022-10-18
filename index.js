d27b11acced647ae68d76e11fbf623e498be20cc
Illia Kozachenko <ilya.s.kozachenko@gmail.com>
2022-10-19 00:27:54 +0300

    yet another CORS config2

--- index.js
+++ index.js
@@ -12,18 +12,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Credentials', true)
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    // another common pattern
-    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-    )
-    next();
-});
 
 app.use(cors());
 app.use(express.json());
